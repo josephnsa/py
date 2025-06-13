4867898d51c43bdd0f35a2fa0fd12b0ff181015e
Crishub280 <salazar.atalayaj@gmail.com>
2025-06-13 16:27:24 -0500

    connection data base

--- sistemaVentasHoteleria/libreria/urls.py
+++ sistemaVentasHoteleria/libreria/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),  
     path('nosotros', views.nosotros, name='nosotros'),
+    path('habitaciones', views.habitaciones, name='habitaciones'),
+    path('habitaciones/crear', views.crear_habitacion, name='crear'),
+    path('habitaciones/editar', views.editar, name='editar'),
+
 ]
--- sistemaVentasHoteleria/libreria/views.py
+++ sistemaVentasHoteleria/libreria/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def inicio(request):
-    return HttpResponse('<h1> Bienvenido Debeloteca</h1>')
+    return render(request, 'paginas/Inicio.html')
 
 def nosotros(request):
     return  render (request, 'paginas/nosotros.html')
+def habitaciones(request):
+    return  render (request, 'habitaciones/index.html')
+def crear_habitacion(request):
+    return render ( request, 'habitaciones/crear.html')
+def editar(request):
+    return render ( request, 'habitaciones/editar.html')
