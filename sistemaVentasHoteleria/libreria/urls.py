4a38be2aedd24f11857920e5c618bbf40c70e7b2
BrendaJimenez215 <brendaashley1508@gmail.com>
2025-06-15 22:19:50 -0500

    Update urls.py

--- sistemaVentasHoteleria/libreria/urls.py
+++ sistemaVentasHoteleria/libreria/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('habitaciones', views.habitaciones, name='habitaciones'),
     path('habitaciones/crear', views.crear_habitacion, name='crear'),
     path('habitaciones/editar', views.editar, name='editar'),
-
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 ]
