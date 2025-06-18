82f217962de3e8924705bf5c3906e9892c7f6f14
Crishub280 <salazar.atalayaj@gmail.com>
2025-06-17 21:47:38 -0500

    create super user

--- sistemaVentasHoteleria/libreria/admin.py
+++ sistemaVentasHoteleria/libreria/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CategoriaCuarto
+from .models import Cuarto
 
 # Register your models here.
+admin.site.register(CategoriaCuarto)
+admin.site.register(Cuarto)
